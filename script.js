53fd14ee4ae23340227bbe7d1de6e280fd72ebe7
saikikkk <ek.marunyak2206@omgau.org>
2024-09-19 12:16:26 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById('loadEpisodes').addEventListener('click', function() {
+    'use strict';
+    
+    var episodeList = document.getElementById('episodeList');
+    
+    // Пример данных эпизодов
+    var episodes = [
+        'Сезон 1, Эпизод 1: Пилот',
+        'Сезон 1, Эпизод 2: Кот в мешке',
+        'Сезон 1, Эпизод 3: ...а мешок в реке',
+        'Сезон 1, Эпизод 4: Раковый больной',
+        'Сезон 1, Эпизод 5: Серое вещество',
+        'Сезон 1, Эпизод 6: Блеф на пустой руке',
+        'Сезон 1, Эпизод 7: Бизнес по-взрослому'
+    ];
+
+     episodeList.innerHTML = '<h3>Эпизоды:</h3>' + episodes.map(episode => `<p>${episode}</p>`).join('');
+});
